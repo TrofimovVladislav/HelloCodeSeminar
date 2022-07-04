ec10cb9e34abf813b1dc6d40ec9a302b688cf9d8
Vladislav Trofimov <arda.yug@gmail.com>
2022-07-04 21:51:42 +0300

    Add Seminar 4

---
+++ Seminar_4/Program.cs
@@ -0,0 +1,58 @@
+﻿// int findeSumNum(int numA)
+// {
+// int result = 0;
+// for(int i = 1; i <=numA; i++)
+// {
+//     result += i;
+// }
+// Console.WriteLine(result);
+// return result;
+// }
+
+// findeSumNum(5);
+
+
+// int digitCount(int n)
+// {
+//     int result = 0;
+//     int div = 1;
+//     while ((n / div) >= 1)
+//     {
+//         div *= 10;
+//         result += 1;
+//     }
+//     Console.WriteLine(result);
+//     return result;
+// }
+
+// digitCount(2);
+// digitCount(12);
+// digitCount(132);
+// digitCount(2456);
+
+// int proizv(int num1)
+// {
+//     int result = 1;
+//     for(int i = 1; i <= num1; i++)
+//     {
+//         result *= i;
+//     }
+    
+//     Console.WriteLine(result);
+//     return result;
+// }
+// proizv(5);
+
+int[] arr()
+{
+    int[] result = new int[8];
+    for(int i=0; i<8; i++)
+    {
+        int A = (new Random()).Next(0,2);
+        result[i]= A;
+        
+Console.Write(result[i] + " ");
+    }
+    return result;
+}
+arr();
