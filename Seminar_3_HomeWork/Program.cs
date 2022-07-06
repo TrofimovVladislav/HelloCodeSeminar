02625f68220847447066b9d84144637c167b42f6
Vladislav Trofimov <arda.yug@gmail.com>
2022-07-07 01:01:39 +0300

    Add commit befor push

--- Seminar_3_HomeWork/Program.cs
+++ Seminar_3_HomeWork/Program.cs
@@ -10,23 +10,23 @@
 23432 -> да
 */
 
-// void isPalindrom(int num)
-// {
-// string numA = num.ToString();
-// char[] arrNumA = numA.ToCharArray();
-// Array.Reverse(arrNumA);
-// string numB = new string(arrNumA);
-// Console.WriteLine(num);
-// if (numA == numB) Console.WriteLine("Это число Палиндром");
-// else Console.WriteLine("Это число не Палиндром");
-// }
+void isPalindrom(int num)
+{
+string numA = num.ToString();
+char[] arrNumA = numA.ToCharArray();
+Array.Reverse(arrNumA);
+string numB = new string(arrNumA);
+Console.WriteLine(num);
+if (numA == numB) Console.WriteLine("Это число Палиндром");
+else Console.WriteLine("Это число не Палиндром");
+}
 
-// isPalindrom(14212);
-// Console.WriteLine();
-// isPalindrom(12821);
-// Console.WriteLine();
-// isPalindrom(23432);
-// Console.WriteLine();
+isPalindrom(14212);
+Console.WriteLine();
+isPalindrom(12821);
+Console.WriteLine();
+isPalindrom(23432);
+Console.WriteLine();
 
 /* -------------------------------------------------------------------------------
 Задача 21
@@ -35,17 +35,17 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
-// double hasLength(double xa, double ya, double za, double xb, double yb, double zb)
-// {
-//     if (xa == xb && ya == yb && za == zb) return 0;
+double hasLength(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    if (xa == xb && ya == yb && za == zb) return 0;
 
-//     double result = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
-//     return result;
-// }
-// double d = hasLength(3, 6, 8, 2, 1, -7);
-// Console.WriteLine(d);
-// double d1 = hasLength(7, -5, 0, 1, -1, 9);
-// Console.WriteLine(d1);
+    double result = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+    return result;
+}
+double d = hasLength(3, 6, 8, 2, 1, -7);
+Console.WriteLine(d);
+double d1 = hasLength(7, -5, 0, 1, -1, 9);
+Console.WriteLine(d1);
 
 /*---------------------------------------------------------
 Задача 23
@@ -58,45 +58,45 @@ A (7,-5, 0); B (1,-1,9) -> 11.53*/
 //------------------------------------------------
 // Вариант 1 (for)
 
-// int[] hasCube(int num)
-// {
-//     Console.WriteLine("Ваше число: " + num);
-//     Console.WriteLine($"Кубы чисел от 1 до {num}:");
-//     int[] numsQube= new int[num];
+int[] hasCube(int num)
+{
+    Console.WriteLine("Ваше число: " + num);
+    Console.WriteLine($"Кубы чисел от 1 до {num}:");
+    int[] numsQube= new int[num];
 
-//     for (int i = 1; i <= num; i++)
-//     {
-//         numsQube[i - 1] = i * i * i;
-//         Console.WriteLine(numsQube[i - 1]);
-//     }
+    for (int i = 1; i <= num; i++)
+    {
+        numsQube[i - 1] = i * i * i;
+        Console.WriteLine(numsQube[i - 1]);
+    }
 
-//     return numsQube;
-// }
+    return numsQube;
+}
 
-// hasCube(3);
-// Console.WriteLine();
-// hasCube(5);
+hasCube(3);
+Console.WriteLine();
+hasCube(5);
 
 //-----------------------------------------------------
 // Вариант 2 (While, Math.Pow)
 
-// int[] findeNumQube(int numN)
-// {
-//     int[] numQube = new int[numN];
-//     int i = 1;
-//     Console.WriteLine("Кубы цифр от 1 до " + numN + ":");
+int[] findeNumQube(int numN)
+{
+    int[] numQube = new int[numN];
+    int i = 1;
+    Console.WriteLine("Кубы цифр от 1 до " + numN + ":");
 
-//     while (i <= numN)
-//     {
-//         numQube[i - 1] = Convert.ToInt32(Math.Pow(i, 3));
-//         Console.Write(numQube[i - 1] + ", ");
-//         i++;
-//     }
+    while (i <= numN)
+    {
+        numQube[i - 1] = Convert.ToInt32(Math.Pow(i, 3));
+        Console.Write(numQube[i - 1] + ", ");
+        i++;
+    }
 
-//     Console.WriteLine();
-//     return numQube;
-// }
+    Console.WriteLine();
+    return numQube;
+}
 
-// findeNumQube(3);
-// findeNumQube(5);
+findeNumQube(3);
+findeNumQube(5);
 
