e76a7582122325da4bb4d6de5d044d5ebc39647d
Vladislav Trofimov <arda.yug@gmail.com>
2022-07-12 07:44:28 +0300

    Add seminar 6

---
+++ Seminar_6/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 39 переворот массива
+
+// int[] arr = new int[] 
+
+
+
+// Задача 40 Проверяет по трем числам может ли быть такой треугольник с такими сторонами.
+// int numA = 100;
+// int numB = 10;
+// int numC = 10;
+
+
+// bool ChekA = numA < numB + numC;
+// bool ChekB = numB < numA + numC;
+// bool ChekC = numC < numA + numC;
+// if (ChekA && ChekB && ChekC)
+// {
+//     Console.WriteLine("Треугольник сущ");
+// }
+// else{
+// Console.WriteLine("Не сущ");
+// }
+
+// Задача 44
+// Не использую рекурсию 
+
+// Задача 45
+
+int [] arr1 = new int [5] {1,2,3,4,5};
+int [] arr2 = new int [5];
+
+for(int i =0; i<arr1.Length;i++)
+{
+arr2[i] = arr1[i];
+Console.Write(arr2[i] + " ");
+}
+
+
