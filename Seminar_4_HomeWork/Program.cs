2c16fb4a1b94161f61f4f3fd04e80a18e965de93
TrofimovVladislav <arda.yug@gmail.com>
2022-07-15 12:43:56 +0300

    Add commit befor pull

--- Seminar_4_HomeWork/Program.cs
+++ Seminar_4_HomeWork/Program.cs
@@ -5,17 +5,17 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
-Console.WriteLine();
-Console.WriteLine("Решение задачи 25:");
-int numberPow(int A, int B)
-{
-    int result = (int)Math.Pow(A, B);
-    Console.WriteLine("Значение числа " + A + " в степени " + B + " = " + result + ";");
-    return result;
-}
+// Console.WriteLine();
+// Console.WriteLine("Решение задачи 25:");
+// int numberPow(int A, int B)
+// {
+//     int result = (int)Math.Pow(A, B);
+//     Console.WriteLine("Значение числа " + A + " в степени " + B + " = " + result + ";");
+//     return result;
+// }
 
-numberPow(3, 5);
-numberPow(2, 4);
+// numberPow(3, 5);
+// numberPow(2, 4);
 
 /*--------------------------------------------------------------------------------------
 Задача 27: Напишите программу, которая принимает 
@@ -24,33 +24,33 @@ numberPow(2, 4);
 82 -> 10
 9012 -> 12
 */
-Console.WriteLine();
-Console.WriteLine("Решение задачи 27:");
-int findSum(int num)
-{
-    int num1 = num, x = 10;
-    while (num1 > 10)
-    {
-        num1 = num1 / x;
-        x *= 10;
-    }
+// Console.WriteLine();
+// Console.WriteLine("Решение задачи 27:");
+// int findSum(int num)
+// {
+//     int num1 = num, x = 10;
+//     while (num1 > 10)
+//     {
+//         num1 = num1 / x;
+//         x *= 10;
+//     }
 
-    int div = x, sum = 0;
-    while (div != 0)
-    {
-        int result = (num / div) % 10;
-        sum = sum + result;
-        div /= 10;
-    }
+//     int div = x, sum = 0;
+//     while (div != 0)
+//     {
+//         int result = (num / div) % 10;
+//         sum = sum + result;
+//         div /= 10;
+//     }
 
-    Console.WriteLine($"Сумма цифр числа {num} = {sum};");
-    return sum;
-}
-findSum(452);
-findSum(82);
-findSum(9012);
+//     Console.WriteLine($"Сумма цифр числа {num} = {sum};");
+//     return sum;
+// }
+// findSum(452);
+// findSum(82);
+// findSum(9012);
 
-Console.WriteLine();
+// Console.WriteLine();
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
@@ -61,43 +61,44 @@ Console.WriteLine();
 // 6, 1, 33 -> [6, 1, 33]
 
 // Вариант 1:
-Console.WriteLine("Решение задачи 29 (Вариант 1):");
-void PrintArray(int N)
-{
-    Console.WriteLine("Искомый массив: ");
+// Console.WriteLine("Решение задачи 29 (Вариант 1):");
+// void PrintArray(int N)
+// {
+//     Console.WriteLine("Искомый массив: ");
 
-    int[] arr = new int[8] { 1, 2, 5, 7, 19, 6, 1, 33 };
-    int i = 1;
+//     int[] arr = new int[8] { 1, 2, 5, 7, 19, 6, 1, 33 };
+//     int i = 1;
 
-    while (i <= N)
-    {
-        Console.Write(arr[i - 1] + ", ");
-        i++;
-    }
+//     while (i <= N)
+//     {
+//         Console.Write(arr[i - 1] + ", ");
+//         i++;
+//     }
 
-    Console.WriteLine();
-}
+//     Console.WriteLine();
+// }
 
-PrintArray(8);
-Console.WriteLine();
+// PrintArray(8);
+// Console.WriteLine();
 // Вариант 2 (Random):
 //-----------------------------------------------------------------------
-Console.WriteLine("Решение задачи 29 (Вариант 2: Random):");
-void PrintArray2(int N)
-{
-    Console.WriteLine("Искомый массив: ");
-    int[] printN = new int[N];
-    int i = 1;
-    Random rand = new Random();
-    while (i <= N)
-    {
-        int numN = rand.Next(1, 99);
-        printN[i - 1] = numN;
-        Console.Write(printN[i - 1] + ", ");
-        i++;
-    }
-    Console.WriteLine();
-}
+// Console.WriteLine("Решение задачи 29 (Вариант 2: Random):");
+// void PrintArray2(int N)
+// {
+//     Console.WriteLine("Искомый массив: ");
+//     int[] printN = new int[N];
+//     int i = 1;
+//     Random rand = new Random();
+//     while (i <= N)
+//     {
+//         int numN = rand.Next(1, 99);
+//         printN[i - 1] = numN;
+//         Console.Write(printN[i - 1] + ", ");
+//         i++;
+//     }
+//     Console.WriteLine();
+// }
 
-PrintArray2(8);
-Console.WriteLine();
+// PrintArray2(8);
+// Console.WriteLine();
+
