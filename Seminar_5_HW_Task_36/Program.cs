a874d5d5235937da50d4acf96dff34b451e108ff
TrofimovVladislav <arda.yug@gmail.com>
2022-07-16 10:32:22 +0300

    Add control commit

--- Seminar_5_HW_Task_36/Program.cs
+++ Seminar_5_HW_Task_36/Program.cs
@@ -6,28 +6,44 @@
 [-4, -6, 89, 6] -> 0
 */
 
-int sumNumbersOddIndex(int length)
+// int sumNumbersOddIndex(int length)
+// {
+//     int[] arrNumbers = new int[length];
+//     Random rnd = new Random();
+
+//     int sumNumOdd = 0;
+
+//     for (int i = 0; i < length; i++)
+//     {
+//         arrNumbers[i] = rnd.Next(-100, 100);
+//         Console.Write(arrNumbers[i] + " ");
+
+//         if (i % 2 != 0) sumNumOdd = sumNumOdd + arrNumbers[i];
+//     }
+
+//     Console.WriteLine();
+//     Console.WriteLine("Сумма элементов с нечетными индексами в заданном массиве = {0}.", sumNumOdd);
+
+//     return sumNumOdd;
+// }
+
+// Console.WriteLine();
+// Console.WriteLine("Решение Задачи № 36:");
+
+// sumNumbersOddIndex(7);
+
+// Вариант Елены
+
+int N = 10;
+int[] arr = new int[N];
+Random rnd = new Random();
+int sum = 0;
+
+for (int i = 0; i < N; i++)
 {
-    int[] arrNumbers = new int[length];
-    Random rnd = new Random();
-
-    int sumNumOdd = 0;
-
-    for (int i = 0; i < length; i++)
+    arr[i] = rnd.Next(100, 1000);
+    if (i % 2)
     {
-        arrNumbers[i] = rnd.Next(-100, 100);
-        Console.Write(arrNumbers[i] + " ");
-
-        if (i % 2 != 0) sumNumOdd = sumNumOdd + arrNumbers[i];
+        sum += arr[i];
     }
-
-    Console.WriteLine();
-    Console.WriteLine("Сумма элементов с нечетными индексами в заданном массиве = {0}.", sumNumOdd);
-
-    return sumNumOdd;
 }
-
-Console.WriteLine();
-Console.WriteLine("Решение Задачи № 36:");
-
-sumNumbersOddIndex(7);
