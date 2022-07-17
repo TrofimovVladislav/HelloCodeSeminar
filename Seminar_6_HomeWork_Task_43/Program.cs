a64dabc3e1258e81353d9fbe12c8a43c45f3a49e
TrofimovVladislav <arda.yug@gmail.com>
2022-07-17 20:49:21 +0300

    Edited description

--- Seminar_6_HomeWork_Task_43/Program.cs
+++ Seminar_6_HomeWork_Task_43/Program.cs
@@ -3,11 +3,6 @@
 //  значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
 
-// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
-// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
-//  значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
-
 // Преобразуем y = k1*x + b1, y = k2*x + b2;
 // k1*x + b1 = k2*x + b2
 // k1*x - k2*x = b2 - b1
@@ -15,12 +10,12 @@
 // x = (b2 - b1)/(k1 - k2)
 // y = x*k1 + b1
 
-// void getCoordinates(double b1, double k1, double b2, double k2)
-// {
-//     double x = (b2 - b1) / (k1 - k2);
-//     double y = x * k2 + b2;
-//     Console.WriteLine("Пересечение {0}, {1}", x, y);
-// }
+void getCoordinates(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = x * k2 + b2;
+    Console.WriteLine("Пересечение {0}, {1}", x, y);
+}
 
-// getCoordinates(2, 5, 4, 9);
+getCoordinates(2, 5, 4, 9);
 
