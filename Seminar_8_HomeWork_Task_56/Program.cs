f4c2be5b6a2b5653bc78e4bb173af8354315d0a4
TrofimovVladislav <arda.yug@gmail.com>
2022-07-21 00:15:55 +0300

    Result Task 56

--- Seminar_8_HomeWork_Task_56/Program.cs
+++ Seminar_8_HomeWork_Task_56/Program.cs
@@ -11,3 +11,40 @@
 // Программа считает сумму элементов в каждой строке и выдаёт 
 // номер строки с наименьшей суммой элементов: 1 строка
 
+int[,] inputArray = new int[3, 4] { { 3, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+int rows = 3;
+int columns = 4;
+void PrintMatrix(int[,] matrix, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(inputArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+PrintMatrix(inputArray, rows, columns);
+
+int sumMin = int.MaxValue; int rowIndex = 0;
+for (int i = 0; i < rows; i++)
+{
+    int sum = 0;
+    for (int j = 0; j < columns; j++)
+    {
+        sum = sum + inputArray[i, j];
+    }
+if(sum < sumMin)
+{
+sumMin = sum;
+rowIndex = i;
+Console.WriteLine("Сумма по строке {0} = {1}", rowIndex, sumMin);
+}
+
+// Console.WriteLine($"Сумма по строке = " + minSum);
+}
+
+
+
